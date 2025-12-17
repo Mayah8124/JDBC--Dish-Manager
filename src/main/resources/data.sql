0820501c7315b2211b028703614a91308abf3f45
mayahNeko <mayahibela@gmail.com>
2025-12-17 14:09:56 +0300

    chore: add insert scripts in data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+CREATE TYPE category AS ENUM ('VEGETABLE', 'ANIMAL' , 'MARINE' , 'DAIRY', 'OTHER');
+
+CREATE TYPE dish_type AS ENUM ('START', 'MAIN', 'DESSERT');
+
+CREATE TABLE Ingredient (
+                            id INT NOT NULL ,
+                            name VARCHAR(100) NOT NULL ,
+                            price NUMERIC(10,2) NOT NULL ,
+                            category category NOT NULL,
+                            dish_id INT,
+                            CONSTRAINT fk_dish FOREIGN KEY (dish_id)
+                                REFERENCES Dish (id)
+);
+
+CREATE TABLE Dish (
+                      id INT CONSTRAINT dish_pk PRIMARY KEY NOT NULL ,
+                      name VARCHAR(100) NOT NULL ,
+                      dish_type dish_type NOT NULL
+);
+
+INSERT INTO Dish (id, name , dish_type) VALUES
+    (1, 'Salade fraiche', 'START'),
+    ('2','Poulet grillé', 'MAIN'),
+    (3,'Riz aux légumes', 'MAIN'),
+    (4, 'Gateau au chocolat' , 'DESSERT'),
+    (5, 'Salade de fruit', 'DESSERT');
+
+INSERT INTO Ingredient (id , name , price , category , dish_id) VALUES
+    (1,'Laitue', 800.00 , 'VEGETABLE', 1),
+    (2,'Tomate' , 600.00, 'VEGETABLE', 1),
+    (3, 'Poulet', 4500.00, 'ANIMAL' , 2),
+    (4 , 'Chocolat', 3000.00, 'OTHER', 4),
+    (5, 'Beurre', 2500.00, 'DAIRY', 4);
