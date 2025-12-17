a1c25a288a7438a9b86378622250e32c79d19ed8
mayahNeko <mayahibela@gmail.com>
2025-12-17 13:59:03 +0300

    chore: add table scripts in schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,19 @@
+CREATE TYPE category AS ENUM ('VEGETABLE', 'ANIMAL' , 'MARINE' , 'DAIRY', 'OTHER');
+
+CREATE TYPE dish_type AS ENUM ('START', 'MAIN', 'DESSERT');
+
+CREATE TABLE Ingredient (
+    id INT NOT NULL ,
+    name VARCHAR(100) NOT NULL ,
+    price NUMERIC(10,2) NOT NULL ,
+    category category NOT NULL,
+    dish_id INT,
+    CONSTRAINT fk_dish FOREIGN KEY (dish_id)
+                        REFERENCES Dish (id)
+);
+
+CREATE TABLE Dish (
+    id INT CONSTRAINT dish_pk PRIMARY KEY NOT NULL ,
+    name VARCHAR(100) NOT NULL ,
+    dish_type dish_type NOT NULL
+);
