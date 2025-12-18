d76aae98764e88ed25d7bb277c374975172be358
mayahNeko <mayahibela@gmail.com>
2025-12-18 14:04:07 +0300

    feat: add new method but not implemented

--- src/main/java/com/JDBC/Data/DataRetriever.java
+++ src/main/java/com/JDBC/Data/DataRetriever.java
@@ -1,5 +1,31 @@
 package com.JDBC.Data;
 
-public class DataRetriever {
+import com.JDBC.*;
 
+import java.util.List;
+
+public class DataRetriever {
+    Dish findDishById(int id) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    List<Ingredient> findIngredients(int page, int size) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    List<Ingredient> createIngredients(List<Ingredient> newIngredients) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    Dish saveDish(Dish dishToSave) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    List<Dish> findDishsByIngredientName(String ingredientName) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    List<Ingredient> findIngredientsByCriteria(String IngredientName, CategoryEnum Category, String dishName, int page, int size) {
+        throw new RuntimeException("Not implemented yet");
+    }
 }
